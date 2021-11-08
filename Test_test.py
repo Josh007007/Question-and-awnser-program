9e4ad3b9f77cac891b00585024aef4d91d963b16
Josh <92541477+Josh007007@users.noreply.github.com>
2021-11-08 20:20:27 +0100

    Update Test_test.py
    Answer to answer

--- Test_test.py
+++ Test_test.py
@@ -16,7 +16,7 @@ while True:
 
     if Answer_1.lower() == RealAnswer_1.lower() or Answer_1.lower() == AlsoRealAnswer_1.lower():
         print('{0}'.format(random.choice(Randomrep_right)))
-    else: print('{0} The Answer was: {1} or {2}'.format(random.choice(Randomrep_wrong), RealAnswer_1, AlsoRealAnswer_1))
+    else: print('{0} The answer was: {1} or {2}'.format(random.choice(Randomrep_wrong), RealAnswer_1, AlsoRealAnswer_1))
     time.sleep(2)
 
     Question_2 = os.getenv('Question_2')
@@ -27,7 +27,7 @@ while True:
 
     if Answer_2.lower() == RealAnswer_2.lower() or Answer_2.lower() == AlsoRealAnswer_2.lower():
         print('{0}'.format(random.choice(Randomrep_right)))
-    else: print('{0} The Answer was: {1} or {2}'.format(random.choice(Randomrep_wrong), RealAnswer_2, AlsoRealAnswer_2))
+    else: print('{0} The answer was: {1} or {2}'.format(random.choice(Randomrep_wrong), RealAnswer_2, AlsoRealAnswer_2))
     time.sleep(2)
     if Answer_1.lower() == RealAnswer_1.lower() or Answer_1.lower() == AlsoRealAnswer_1.lower() and Answer_2.lower() == RealAnswer_2.lower() or Answer_2.lower() == AlsoRealAnswer_2.lower():
         break
