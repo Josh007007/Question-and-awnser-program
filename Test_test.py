5011e35bf57bb09e0ecf0439ff7e331b6bfce142
Josh <92541477+Josh007007@users.noreply.github.com>
2021-11-12 17:42:51 +0100

    Answer 3 beginning

--- Test_test.py
+++ Test_test.py
@@ -35,17 +35,33 @@ while True:
         print('\033[1;31;40m{0} The answer was: {1} or {2}'.format(random.choice(Randomrep_wrong), RealAnswer_2, AlsoRealAnswer_2))
         time.sleep(1.5)
 
+    Question_3 = os.getenv('Question_3')
+    RealAnswer_3 = os.getenv('RealAnswer_3')
+    AlsoRealAnswer_3 = os.getenv('AlsoRealAnswer_3')
+    Answer_3 = input('\033[1;33;40m{0}: '.format(Question_3))
+
+    if Answer_3.lower() == RealAnswer_3.lower() or Answer_3.lower() == AlsoRealAnswer_3.lower():
+        print('\033[1;32;40m{0}'.format(random.choice(Randomrep_right)))
+    elif RealAnswer_3 == AlsoRealAnswer_3: 
+        print('\033[1;31;40m{0} The only answer was: {1}'.format(random.choice(Randomrep_wrong), RealAnswer_3))
+    else:
+        print('\033[1;31;40m{0} The answer was: {1} or {2}'.format(random.choice(Randomrep_wrong), RealAnswer_3, AlsoRealAnswer_3))
+        time.sleep(1.5)
+
     Ans_1RealAns_1 = 'Answer_1.lower() == RealAnswer_1.lower()'
     Ans_1AlsoRealAns_1 = 'Answer_1.lower() == AlsoRealAnswer_1.lower()'
     Ans_2RealAns_2 = 'Answer_2.lower() == RealAnswer_2.lower()'
     Ans_2AlsoRealAns_2 = 'Answer_2.lower() == AlsoRealAnswer_2.lower()'
+    Ans_3RealAns_3 = 'Answer_3.lower() == RealAnswer_3.lower()'
+    Ans_3AlsoRealAns_3 = 'Answer_2.lower() == AlsoRealAnswer_2.lower()'
 
-    Ans_all = 'eval(Ans_1RealAns_1) or eval(Ans_1AlsoRealAns_1) or eval(Ans_2RealAns_2) or eval(Ans_2AlsoRealAns_2)'
+    Ans_all1 = 'eval(Ans_1RealAns_1) or eval(Ans_1AlsoRealAns_1) or eval(Ans_2RealAns_2) or eval(Ans_2AlsoRealAns_2) or eval(Ans_3RealAns_3) or eval(Ans_3AlsoRealAns_3)'
 
     Ans_1RealAns_1Ans_2RealAns_2 = 'Answer_1.lower() == RealAnswer_1.lower() and Answer_2.lower() == RealAnswer_2.lower()'
     Ans_1AlsoRealAns_1Ans_2RealAns_2 = 'Answer_1.lower() == AlsoRealAnswer_1.lower() and Answer_2.lower() == RealAnswer_2.lower()'
     Ans_1RealAns_1Ans_2AlsoRealAns_2 = 'Answer_1.lower() == RealAnswer_1.lower() and Answer_2.lower() == AlsoRealAnswer_2.lower()'
     Ans_1AlsoRealAns_1Ans_2AlsoRealAns_2 = 'Answer_1.lower() == AlsoRealAnswer_1.lower() and Answer_2.lower() == AlsoRealAnswer_2.lower()'
+    #Ans_3AlsoRealAns_3!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
     if eval(Ans_1RealAns_1Ans_2RealAns_2) or eval(Ans_1AlsoRealAns_1Ans_2RealAns_2) or eval(Ans_1RealAns_1Ans_2AlsoRealAns_2) or eval(Ans_1AlsoRealAns_1Ans_2AlsoRealAns_2):
         print("\033[1;32;40mYou got all of the answers right!")
@@ -53,7 +69,7 @@ while True:
         Replay = input("\033[1;33;40mDo you want to continue? (Y or N): ")
         if Replay.upper() == "N":
             break
-    elif eval(Ans_all):
+    elif eval(Ans_all1):
         print("\033[1;32;40mYou got 1 answer right.")
         time.sleep(0.5)
         Replay = input("\033[1;33;40mDo you want to continue? (Y or N): ")
