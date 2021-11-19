85f70d9e2a7659566a019935166fcf2c5f104db3
Josh <92541477+Josh007007@users.noreply.github.com>
2021-11-19 16:35:40 +0100

    Now the code is shorter
    Now asks how many questions there are, what makes the code much shorter and makes it so you can freely add questions to the .env file.

--- Test_test.py
+++ Test_test.py
@@ -4,67 +4,59 @@ import random
 from dotenv import load_dotenv
 Score_Total = 0
 Score_Max_Total = 0
+Num = int(0)
+while True:
+    Num = int(input("How many questions are there in the .env file? (Default is 3): "))
+    if Num <= 0:
+        print("Error 404 Questions not found.\nPlease import a number higher than 0.")
+        time.sleep(1)
+    else: 
+        break
+
+Num_list = []
+Num_Counting = 0
+Num_list_Counting = 0
+
+while True:
+    Num_list.append(Num_Counting)
+    if Num_Counting == Num:
+        break
+    Num_Counting += 1
+
 
 while True:
     Score = 0
     Score_Max = 0
     Randomrep_wrong = ('I\'m sorry to inform you that you\'re wrong.', 'You almost had it!', 'Try better next time.', 'Error 404: right Answer not found.', 'Keep determined.')
     Randomrep_right = ('DETERMINATION', 'Good job', 'You won...\nBut at what cost?', 'Mission complete\nRespect +', 'Bigbrain moment')
+    Num_list_Counting = 1
+    while True:
+        Question = 'Question_{0}'.format(Num_list[Num_list_Counting])
+        RealAnswer = 'RealAnswer_{0}'.format(Num_list[Num_list_Counting])
+        AlsoRealAnswer = 'AlsoRealAnswer_{0}'.format(Num_list[Num_list_Counting])
+        #Tip = 0!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-    load_dotenv()
-    Question_1 = os.getenv('Question_1')
-    Answer_1 = input('\033[1;33;40m{0}: '.format(Question_1))
-    Score_Max += 1
+        load_dotenv()
+        Question = os.getenv(Question)
+        Answer = input('\033[1;33;40m{0}: '.format(Question))
+        Score_Max += 1
 
-    RealAnswer_1 = os.getenv('RealAnswer_1')
-    AlsoRealAnswer_1 = os.getenv('AlsoRealAnswer_1')
+        RealAnswer = os.getenv(RealAnswer)
+        AlsoRealAnswer = os.getenv(AlsoRealAnswer)
 
-    if Answer_1.lower() == RealAnswer_1.lower() or Answer_1.lower() == AlsoRealAnswer_1.lower():
-        print('\033[1;32;40m{0}'.format(random.choice(Randomrep_right)))
-        Score += 1
-        time.sleep(1)
-    elif RealAnswer_1 == AlsoRealAnswer_1: 
-        print('\033[1;31;40m{0} The only answer was: {1}'.format(random.choice(Randomrep_wrong), RealAnswer_1))
-        time.sleep(1.5)
-    else: 
-        print('\033[1;31;40m{0} The answers were: {1} or {2}'.format(random.choice(Randomrep_wrong), RealAnswer_1, AlsoRealAnswer_1))
-        time.sleep(1.5)
-
-    Question_2 = os.getenv('Question_2')
-    Answer_2 = input('\033[1;33;40m{0}: '.format(Question_2))
-    Score_Max += 1
-
-    RealAnswer_2 = os.getenv('RealAnswer_2')
-    AlsoRealAnswer_2 = os.getenv('AlsoRealAnswer_2')
-
-    if Answer_2.lower() == RealAnswer_2.lower() or Answer_2.lower() == AlsoRealAnswer_2.lower():
-        print('\033[1;32;40m{0}'.format(random.choice(Randomrep_right)))
-        Score += 1
-        time.sleep(1)
-    elif RealAnswer_2 == AlsoRealAnswer_2: 
-        print('\033[1;31;40m{0} The only answer was: {1}'.format(random.choice(Randomrep_wrong), RealAnswer_2))
-        time.sleep(1.5)
-    else:
-        print('\033[1;31;40m{0} The answers were: {1} or {2}'.format(random.choice(Randomrep_wrong), RealAnswer_2, AlsoRealAnswer_2))
-        time.sleep(1.5)
-
-    Question_3 = os.getenv('Question_3')
-    RealAnswer_3 = os.getenv('RealAnswer_3')
-
-    AlsoRealAnswer_3 = os.getenv('AlsoRealAnswer_3')
-    Answer_3 = input('\033[1;33;40m{0}: '.format(Question_3))
-    Score_Max += 1
-
-    if Answer_3.lower() == RealAnswer_3.lower() or Answer_3.lower() == AlsoRealAnswer_3.lower():
-        print('\033[1;32;40m{0}'.format(random.choice(Randomrep_right)))
-        Score += 1
-        time.sleep(1)
-    elif RealAnswer_3 == AlsoRealAnswer_3: 
-        print('\033[1;31;40m{0} The only answer was: {1}'.format(random.choice(Randomrep_wrong), RealAnswer_3))
-        time.sleep(1.5)
-    else:
-        print('\033[1;31;40m{0} The answers were: {1} or {2}'.format(random.choice(Randomrep_wrong), RealAnswer_3, AlsoRealAnswer_3))
-        time.sleep(1.5)
+        if Answer.lower() == RealAnswer.lower() or Answer.lower() == AlsoRealAnswer.lower():
+            print('\033[1;32;40m{0}'.format(random.choice(Randomrep_right)))
+            Score += 1
+            time.sleep(1)
+        elif RealAnswer == AlsoRealAnswer: 
+            print('\033[1;31;40m{0} The only answer was: {1}'.format(random.choice(Randomrep_wrong), RealAnswer))
+            time.sleep(1.5)
+        else: 
+            print('\033[1;31;40m{0} The answers were: {1} or {2}'.format(random.choice(Randomrep_wrong), RealAnswer, AlsoRealAnswer))
+            time.sleep(1.5)
+        if Num == Num_list[Num_list_Counting]:
+            break
+        Num_list_Counting += 1
 
     Score_Total += Score
     Score_Max_Total += Score_Max
@@ -86,5 +78,5 @@ while True:
         if Replay.upper() == "N":
             break
 time.sleep(0.6)
-print("\033[3;30;47mSee you next time", "\033[1;37;40m")
+print("\033[3;30;47mSee you next time")
 time.sleep(1)
